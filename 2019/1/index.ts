3a0355459c0ff7d1de7784f5f041a93ee2b1582b
NiewView <tom.eckardt.it@gmail.com>
2020-12-01 06:16:37 +0000

    loaded new challenge from advent of code

---
+++ 2019/1/index.ts
@@ -0,0 +1,24 @@
+export function calculate(mass: number) {
+  const result = Math.floor(mass / 3) - 2;
+  return Math.max(result, 0);
+}
+
+export async function challenge1() {
+  const input: string = Deno.readTextFileSync("input.txt");
+  const stringInputArray: Array<string> = input.split("\n");
+  const numberInputArray: Array<number> = input
+    .split("\n")
+    .map((item) => Number(item));
+
+  calculate(numberInputArray[0]);
+}
+
+export async function challenge2() {
+  const input: string = Deno.readTextFileSync("input.txt");
+  const stringInputArray: Array<string> = input.split("\n");
+  const numberInputArray: Array<number> = input
+    .split("\n")
+    .map((item) => Number(item));
+
+  return calculate(numberInputArray[0]);
+}
