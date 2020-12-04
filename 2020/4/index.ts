51c88cbc3fe683ca896d0e62e3811cfbe7893638
Tom <tom.eckardt.it@gmail.com>
2020-12-04 14:45:27 +0100

    day 4 part2 resolve #10

--- 2020/4/index.ts
+++ 2020/4/index.ts
@@ -11,13 +11,14 @@ export interface Passport {
 
 export function countValidPassports(
   input: string,
-  ignoreProps: Array<string>
+  ignoreProps: Array<string>,
+  validateFields = false
 ): number {
   let count = 0;
   const passwordStrings = splitPassports(input);
   for (const passwordString of passwordStrings) {
     const password = parsePassport(passwordString);
-    const isValid = isPassportValid(password, ignoreProps);
+    const isValid = isPassportValid(password, ignoreProps, validateFields);
     if (isValid) {
       count += 1;
     }
@@ -38,18 +39,64 @@ export const passportTemplate: Passport = {
 
 export function isPassportValid(
   passport: Passport,
-  ignoreProps: Array<string>
+  ignoreProps: Array<string>,
+  validateFields = false
 ): boolean {
   const necessaryProps = Object.keys(passportTemplate).filter(
     (item) => !ignoreProps.includes(item)
   );
 
   for (const necessaryProp of necessaryProps) {
-    if (passport[necessaryProp as keyof Passport] === null) {
+    const value = passport[necessaryProp as keyof Passport];
+    if (value === null) {
+      return false;
+    }
+    if (validateFields && !isPassportFieldValueValid(necessaryProp, value)) {
       return false;
     }
   }
   return true;
+}
+
+export function isPassportFieldValueValid(field: string, value: string) {
+  /* eslint-disable no-case-declarations */
+  try {
+    switch (field) {
+      case "byr":
+        const byr = Number(value);
+        return byr >= 1920 && byr <= 2002;
+      case "iyr":
+        const iyr = Number(value);
+        return iyr >= 2010 && iyr <= 2020;
+      case "eyr":
+        const eyr = Number(value);
+        return eyr >= 2020 && eyr <= 2030;
+      case "hgt":
+        const hgt_number = Number(value.match(/^(\d+)[a-zA-Z]+$/)![1]);
+        const hgt_unit = value.match(/^\d+([a-zA-Z]+)$/)![1];
+        if (hgt_unit === "cm") {
+          return !isNaN(hgt_number) && hgt_number >= 150 && hgt_number <= 193;
+        }
+        if (hgt_unit === "in") {
+          return !isNaN(hgt_number) && hgt_number >= 49 && hgt_number <= 76;
+        }
+        return false;
+      case "hcl":
+        return /^#[a-f0-9]{6}$/.test(value);
+      case "ecl":
+        const ecl_values = ["amb", "blu", "brn", "gry", "grn", "hzl", "oth"];
+        return ecl_values.includes(value);
+      case "pid":
+        return /^\d{9}$/.test(value);
+      case "cid":
+        return true;
+      default:
+        return false;
+      /* eslint-enable no-case-declarations */
+    }
+  } catch (error) {
+    return false;
+  }
 }
 
 export function splitPassports(input: string): Array<string> {
@@ -93,5 +140,5 @@ export async function challenge2() {
     .split("\n")
     .map((item) => Number(item));
 
-  // return isPassportValid(numberInputArray[0]);
+  return countValidPassports(input, ["cid"], true);
 }
